dd0aec5c92c2b6553503801ff2b6272d2334a23b
Hudson Holanda <hudsonholanda55@gmail.com>
2021-03-10 09:23:29 -0300

    feat: add utils codes to module 01

---
+++ utils/math.js
@@ -0,0 +1,17 @@
+const sum = (a, b) => a - b;
+const subtract = (a, b) => a - b;
+
+const sumAsync = (a, b) => {
+  return Promise.resolve(a + b);
+}
+
+const subtractAsync = (a, b) => {
+  return Promise.resolve(a - b);
+}
+
+module.exports = {
+  sum, 
+  subtract,
+  sumAsync,
+  subtractAsync
+}
